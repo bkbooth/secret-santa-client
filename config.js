3dfad14adcd45684fbb081b70ed1cd1bc11d721d
Ben Booth <bkbooth@gmail.com>
2018-11-19 23:51:00 +1100

    Re-add the domain name emojifier

---
+++ config.js
@@ -0,0 +1,3 @@
+export const EMOJI_DOMAINS = ['🤫🎅.ws', '🤫🎁.ws']
+export const PUNYCODE_DOMAINS = ['xn--8j8hl4g.ws', 'xn--4j8ht4g.ws']
+export const STANDARD_DOMAINS = ['shhh.gift']
---
+++ pages/_app.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import App, { Container } from 'next/app'
+
+import { emojifyDomain } from '../lib/emojifyDomain'
+try {
+  emojifyDomain(window.location)
+} catch (err) {}
+
+export default class MyApp extends App {
+  static async getInitialProps({ Component, router, ctx }) {
+    let pageProps = {}
+
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx)
+    }
+
+    return { pageProps }
+  }
+
+  render () {
+    const { Component, pageProps } = this.props
+
+    return (
+      <Container>
+        <Component {...pageProps} />
+      </Container>
+    )
+  }
+}
