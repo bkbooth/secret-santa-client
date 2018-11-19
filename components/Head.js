9b47514cc9bb8ebf3ed7052484cb87d25043ab6d
Ben Booth <bkbooth@gmail.com>
2018-11-20 00:04:36 +1100

    Fix http-equiv attribute

--- components/Head.js
+++ components/Head.js
@@ -5,7 +5,7 @@ const Head = () => (
   <NextHead>
     <meta charSet="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
     <link rel="icon" href="/static/favicon.ico" />
 
     <title>🤫🎁</title>
