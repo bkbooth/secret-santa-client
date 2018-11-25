fea95b63b71d6cdf1abc3e3a772f081a8c2b6683
Ben Booth <bkbooth@gmail.com>
2018-11-25 23:24:02 +1100

    Add prettier and setup to autoformat

--- next.config.js
+++ next.config.js
@@ -1,15 +1,16 @@
 module.exports = {
   publicRuntimeConfig: {
-    emojiDomains: ['🤫🎅.ws', '🤫🎁.ws'],
-    punycodeDomains: ['xn--8j8hl4g.ws', 'xn--4j8ht4g.ws'],
-    standardDomains: ['shhh.gift'],
+    emojiDomains: ["🤫🎅.ws", "🤫🎁.ws"],
+    punycodeDomains: ["xn--8j8hl4g.ws", "xn--4j8ht4g.ws"],
+    standardDomains: ["shhh.gift"]
   },
+
   webpack: config => {
     // Fixes npm packages that depend on `fs` module
     config.node = {
-      fs: 'empty'
-    }
+      fs: "empty"
+    };
 
-    return config
+    return config;
   }
-}
+};
